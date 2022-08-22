6d19312539493eab023132b17175e3846557e1c5
lstprjct <88411318+lstprjct@users.noreply.github.com>
2022-08-22 23:36:25 +0600

    Create IAS.ps1

---
+++ IAS.ps1
@@ -0,0 +1,19 @@
+# Enable TLSv1.2 for compatibility with older clients
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor [System.Net.SecurityProtocolType]::Tls12
+
+$DownloadURL = 'https://raw.githubusercontent.com/lstprjct/IDM-Activation-Script/main/IAS_0.7_ModByPiash.cmd'
+
+$FilePath = "$env:TEMP\IAS.cmd"
+
+try {
+    Invoke-WebRequest -Uri $DownloadURL -UseBasicParsing -OutFile $FilePath
+} catch {
+    Write-Error $_
+	Return
+}
+
+if (Test-Path $FilePath) {
+    Start-Process $FilePath -Wait
+    $item = Get-Item -LiteralPath $FilePath
+    $item.Delete()
+}
