9574ca2f4e5452be39b46a43636f04e517254370
яυzαιηι <ruzaini@ourtech.my>
2023-02-19 20:48:38 +0800

    Update IAS.ps1

--- IAS.ps1
+++ IAS.ps1
@@ -1,7 +1,7 @@
 # Enable TLSv1.2 for compatibility with older clients
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor [System.Net.SecurityProtocolType]::Tls12
 
-$DownloadURL = 'https://raw.githubusercontent.com/lstprjct/IDM-Activation-Script/main/IAS_0.7_ModByPiash.cmd'
+$DownloadURL = 'https://raw.githubusercontent.com/z4q/IDM-Activation-Script/main/IAS_0.7_ModByPiash.cmd'
 
 $FilePath = "$env:TEMP\IAS.cmd"
 
